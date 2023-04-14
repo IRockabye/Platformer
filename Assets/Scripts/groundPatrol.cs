7610ae705c65b026a35743cbeb9a233d96305edd
Захар Кузовлев <kuzovlev.96@bk.ru>
2023-04-14 12:22:43 +0700

    0000

---
+++ Assets/Scripts/groundPatrol.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class groundPatrol : MonoBehaviour
+{
+
+    public float speed = 1.5f;
+    public bool moveleft = true;
+    public Transform groundDetect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetect.position, Vector2.down, 0.4f);
+
+        if (groundInfo.collider == false)
+        {
+            if (moveleft == true)
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+                moveleft = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                moveleft = true;
+            }
+        }
+    }
+}
